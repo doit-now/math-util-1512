56d72387e91e6619aae9fd30e1b863c61d9d8cc4
doit-now <hoang.nguyenthe@gmail.com>
2021-09-28 10:22:43 +0700

    add a new test case in Advanced...

--- test/com/giaolang/mathutil/core/AdvancedFactorialTest.java
+++ test/com/giaolang/mathutil/core/AdvancedFactorialTest.java
@@ -48,6 +48,7 @@ public class AdvancedFactorialTest {
                                   {3, 6},
                                   {4, 24},
                                   {5, 120},
+                                  {6, 720}, //bổ sung test CI
                                };
     }
     
