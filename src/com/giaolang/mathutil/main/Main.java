f6d512b79b285e6959ca05fe7ab5a7562cd38f3e
doit-now <hoang.nguyenthe@gmail.com>
2021-09-30 08:15:57 +0700

    modify on main() and merge

--- src/com/giaolang/mathutil/main/Main.java
+++ src/com/giaolang/mathutil/main/Main.java
@@ -19,10 +19,11 @@ public class Main {
         System.out.println("0! = 1? " + MathUtil.getFactorial(0));
         System.out.println("1! = 1? " + MathUtil.getFactorial(1));
         System.out.println("5! = 120? " + MathUtil.getFactorial(5));
-        System.out.println("6! = 720? " + MathUtil.getFactorial(6));  //modify by someone before
+        System.out.println("6! = 720? " + MathUtil.getFactorial(6));  //approved
+
     }
     
-    //modify from local
+    //approved
     public static void testFactorialSuccessfullCases() {
         System.out.println("0! = 1? " + MathUtil.getFactorial(0));
     }
