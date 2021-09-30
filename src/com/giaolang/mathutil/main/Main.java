c547117909fb7fb05d749c88ba57f73b01b05466
doit-now <hoang.nguyenthe@gmail.com>
2021-09-30 08:10:04 +0700

    modify on main()

--- src/com/giaolang/mathutil/main/Main.java
+++ src/com/giaolang/mathutil/main/Main.java
@@ -20,6 +20,11 @@ public class Main {
         System.out.println("1! = 1? " + MathUtil.getFactorial(1));
         System.out.println("5! = 120? " + MathUtil.getFactorial(5));
     }
+    
+    //modify from local
+    public static void testFactorialSuccessfullCases() {
+        System.out.println("0! = 1? " + MathUtil.getFactorial(0));
+    }
 }
 
 //tự nhìn, tự so sánh, tự kết luận cho từng trường hợp và cho all
